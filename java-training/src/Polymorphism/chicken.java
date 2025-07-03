33e5ee1b47e60dc79e7aa29ab0d20fcfcd909dbf
postmaduyen <leanhduc281003@gmail.com>
2025-07-03 13:38:24 +0700

    polymorphism

---
+++ java-training/src/Polymorphism/chicken.java
@@ -0,0 +1,42 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Polymorphism;
+
+
+
+class animal {
+    public void weight(int a, int b) {
+        System.out.println(a+b);
+    }
+    
+    public void weight(int a, int b, int c) {
+        System.out.println(a+b+c);
+    }
+    
+    public void weight(double a, double b) {
+        System.out.println(a+b);
+    }
+    public void legs(){
+            System.out.println("This animal has four legs");
+    }
+}
+public class chicken extends animal{
+
+    public chicken() {
+    }
+    
+    @Override    
+    public void legs(){
+        System.out.println("This animal has two legs");
+    }
+    
+    public static void main(String[] args) {
+        chicken c = new chicken();
+        c.weight(1, 2);
+        c.weight(1, 2, 3);
+        c.weight(1.5, 2.5);
+        c.legs();
+    }
+}
